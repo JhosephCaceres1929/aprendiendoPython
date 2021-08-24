515388e2bff188bed8d0ff7031c026b5c0b760b8
Jhoseph <jhoseph@localhost.localdomain>
2021-08-24 16:46:21 -0400

    ejercicio eliminando espacio

---
+++ clase1/eliminar espacios.py
@@ -0,0 +1,9 @@
+#usr/python3/bin
+
+comentario ="    los animales de hoy en dia     estan en extincion    "
+
+print (comentario.lstrip());#elimina espacio izquierda
+print(comentario.split())#pone en comilla cada palabra
+print(comentario.strip())#elimina todos los espacios
+print(comentario.rstrip())#elimina los espacion en la derecha
+print (comentario)
